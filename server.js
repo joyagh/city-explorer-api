451624354d1cc563fdf5b2249461684e6669b631
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-07-29 14:30:42 -0500

    added dotenv config

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require ('express');
+const express = require('express');
 const cors = require ('cors');
 
 const app = express();
 app.use(cors());
+
+require('dotenv').config(); 
 
 const PORT = process.env.PORT;
 

b7c67f523c04b96dcdef8a956af86a8ce0ee679d
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-08-03 18:23:53 -0500

    commit

--- server.js
+++ server.js
@@ -1,34 +1,55 @@
-const express = require('express');
-const cors = require ('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
-require('dotenv').config(); 
+require("dotenv").config();
 
 const PORT = process.env.PORT;
 
-const data = require('./data/weather.json');
+const data = require("./data/weather.json");
 
 class Forecast {
-     constructor(weatherObj) {
-          this.date = weatherObj.date;
-          this.description = weatherOnj.description;
-     }
+  constructor(valid_date, description ) {
+    this.date = valid_date;
+    this.description = description;
+  }
 }
 
-app.get('/', (request, response) => {
-     response.send("City Explorer!");
-})
+app.get("/", (request, response) => {
+  response.send("City Explorer!");
+});
 
-app.get('/weather', (request, response) => {
-     response.send(data);
-})
-    
+app.get("/weather", (request, response) => {
+  let req = {
+    query: {
+      lat: "47.60621",
+      lon: "-122.33207",
+      searchQuery: "Seattle",
+    },
+  }; try{
+  let result = data.find(
+    (city) =>
+      city.city_name === request.query.searchQuery && city.lat === request.query.lat
+  );
+  let dates = result.data.map(
+     (day) => {
+     let forecast = new Forecast(day.valid_date, day.weather.description)
+     console.log(day);
+     return forecast;
+     })
+  response.send(dates);
+  }catch (error){
+     response.status(404).send({error: "City not found."})
+  }
+});
 
-
-
+// "city_name": "Seattle",
+//      "lon": "-122.33207",
+//      "timezone": "America/Los_Angeles",
+//      "lat": "47.60621",
 
 app.listen(PORT, () => {
-     console.log(`${PORT}`);
+  console.log(`${PORT}`);
 });

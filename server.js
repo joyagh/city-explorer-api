e252bd868d87f9d583323d96815f326961297152
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-08-09 07:41:35 -0500

    routed to front end

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const apiKey = process.env.WEATHER_API_KEY;
 const PORT = process.env.PORT;
 const movieKey = process.env.MOVIE_API_KEY;
 
-const data = require("./data/weather.json");
+// const data = require("./data/weather.json");
 
 class Forecast {
   constructor(valid_date, description) {
@@ -20,31 +20,38 @@ class Forecast {
 }
 const proofOfLife = (request, response) => {
   response.send("City Explorer!");
-}
+};
 app.get("/", proofOfLife);
 
 app.get("/weather", async (request, response) => {
-     // http://localhost:4001/weather
-
+  // http://localhost:4001/weather?lon=-122.33207&lat=47.60621
+  console.log(request.query);
   try {
-    let result = data.find((city) =>
-        city.city_name === request.query.searchQuery);
-
-    if (result) {
-    let dates = result.data.map((day) => {
-      let forecast = new Forecast(day.valid_date, day.weather.description);
-      // console.log(day);
-      return forecast;
-    });
-    response.send(dates);
-  } else {
+    const weatherData = await axios.get(
+      `https://api.weatherbit.io/v2.0/forecast/daily?key=${apiKey}&days=16&lat=${request.query.lat}&lon=${request.query.lon}`
+    );
+    
+    if (weatherData) {
+      /* let dates = []
+      for (let i = 0; i < 10; i++){
+        dates.push(new Forecast(weatherData.data.data[i].datetime, weatherData.data.data[i].weather.description))
+      }
+      */
+      let dates = weatherData.data.data.map((day) => {
+        let forecast = new Forecast(day.datetime, day.weather.description);
+        // console.log(day);
+        return forecast;
+      });
+      console.log(dates);
+      response.send(dates);
+    } else {
+      response.status(404).send({ error: "City not found." });
+    }
+  } catch (error) {
     response.status(404).send({ error: "City not found." });
   }
-  } catch (error) {
-    response.status(500).send({error: "An error has occured"});
-  }
 });
-// app.get("./movies" (request, response) => {
+// app.get("/movies" (request, response) => {
 
 // })
 
